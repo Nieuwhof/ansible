52df63fd2eefc39580291a32ee95617f8909cba3
Bertus Nieuwhof <bnieuwhof@gmail.com>
2024-02-07 07:36:50 -0500

    qeg

--- python/demo.py
+++ python/demo.py
@@ -1,8 +1,37 @@
+import random
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
 
-# Slots
+ROWS =  3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for col in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(all_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+    columns.append(column)
+
 def deposit():
     while True:
         amount = input("Enter amount to be deposited: E")
@@ -60,6 +89,7 @@ def main():
 
     print(f"You are betting ${bet} on {lines} lines... Total bet: ${total_bet}...")
 
+tspin = get_slot_machine_spin(ROWS, COLS, symbol_count.keys())
 
 
 main()
