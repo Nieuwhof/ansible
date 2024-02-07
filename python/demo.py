5fbe8c6ae70e8e61d30fd6b4960dc55c93ca6b9a
Bertus Nieuwhof <bnieuwhof@gmail.com>
2024-02-07 09:34:51 -0500

    nearly final

--- python/demo.py
+++ python/demo.py
@@ -38,9 +38,9 @@ def print_slot_machine(columns):
     for row in range(len(columns[0])):
         for i, column in enumerate(columns):
             if i != len(columns) - 1:
-                print(column[row],"|")
+                print(column[row],end="|")
             else:
-                print(column[row])
+                print(column[row],end="\n")
 
 
 def deposit():
@@ -99,7 +99,7 @@ def main():
             break
 
     print(f"You are betting ${bet} on {lines} lines... Total bet: ${total_bet}...")
-)
+
 
 slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
 print_slot_machine(slots)
