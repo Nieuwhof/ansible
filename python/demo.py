1460381bb548d93843b6e3e49c2e7b39b0667914
Bertus Nieuwhof <bnieuwhof@gmail.com>
2024-02-09 10:32:11 -0500

    yyy

--- python/demo.py
+++ python/demo.py
@@ -1,109 +1,17 @@
-import random
+import os
 
-MAX_LINES = 3
-MAX_BET = 100
-MIN_BET = 1
+PATH = '/home/bertus/demo/files'
 
-ROWS =  3
-COLS = 3
-
-symbol_count = {
-    "A": 2,
-    "B": 4,
-    "C": 6,
-    "D": 8
-}
+# Create the directory
+isExist = os.path.exists(PATH)
+if isExist == True:
+    print('Directory already exists')
+else:
+    os.mkdir(PATH)
+    print('Directory created')
 
 
-def get_slot_machine_spin(rows, cols, symbols):
-    all_symbols = []
-    for symbol, symbol_count in symbols.items():
-        for _ in range(symbol_count):
-            all_symbols.append(symbol)
+#    os.mkdir(PATH)
 
-    columns = []
-    for col in range(cols):
-        column = []
-        current_symbols = all_symbols[:]
-        for _ in range(rows):
-            value = random.choice(all_symbols)
-            current_symbols.remove(value)
-            column.append(value)
-
-        columns.append(column) 
-
-    return columns
-
-def print_slot_machine(columns):
-    for row in range(len(columns[0])):
-        for i, column in enumerate(columns):
-            if i != len(columns) - 1:
-                print(column[row],end="|")
-            else:
-                print(column[row],end="\n")
-
-
-def deposit():
-    while True:
-        amount = input("Enter amount to be deposited: E")
-        if amount.isdigit():
-            amount = int(amount)
-            if amount > 0:
-                break
-            else:
-                print("Amount must be greater than zero...")
-        else:
-            print("Amount must be a number!")
-
-    return amount
-
-def get_number_of_lines():
-    while True:
-        lines = input("Enter number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
-        if lines.isdigit():
-            lines  = int(lines)
-            if 1 <= lines <= MAX_LINES:
-                break
-            else:
-                print("Enter valid amount of lines...")
-        else:
-            print("Amount must be a number!")
-
-    return lines
-
-def get_bet():
-    while True:
-        amount = input("Enter amount to be bet on each line: E")
-        if amount.isdigit():
-            amount = int(amount)
-            if MIN_BET <= amount <= MAX_BET:
-                break
-            else:
-                print(f"Amount must be between ${MIN_BET} - ${MAX_BET} ...")
-        else:
-            print("Amount must be a number!")
-
-    return amount    
-
-
-def main():
-    balance = deposit()
-    lines = get_number_of_lines()
-    while True:
-        bet = get_bet()
-        total_bet = lines * bet
-
-        if total_bet > balance:
-            print(f"Insufficient funds... Your balance is ${balance}...")
-        else:
-            break
-
-    print(f"You are betting ${bet} on {lines} lines... Total bet: ${total_bet}...")
-
-
-slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
-print_slot_machine(slots)
-
-
-main()
-
+# Create a file
+#with open(os.path.join(PATH, 'file.txt'), 'w') as file:
