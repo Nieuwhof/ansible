b5015e44da9b8d4d4cb6d573a3e9817ff1854269
Lambertus Nieuwhof <bnieuwhof@gmail.com>
2024-02-10 12:27:43 +0330

    create random files

--- python/demo.py
+++ python/demo.py
@@ -1,6 +1,6 @@
 import os
 
-PATH = '/home/bertus/demo/files'
+PATH = 'files'
 
 # Create the directory
 isExist = os.path.exists(PATH)
@@ -10,8 +10,15 @@ else:
     os.mkdir(PATH)
     print('Directory created')
 
+# Create 100 files
+extentions = ['.txt', '.pdf', '.docx', '.xlsx', '.pptx']
+for i in range(1, 101):
+    file_name = f'file_{i}{extentions[i%5]}'
+    with open(f'{PATH}/{file_name}', 'w') as file:
+        file.write(f'This is the content of {file_name}')
+        print(f'{file_name} created')   
+print('All files created')
 
-#    os.mkdir(PATH)
 
-# Create a file
-#with open(os.path.join(PATH, 'file.txt'), 'w') as file:
+
+
